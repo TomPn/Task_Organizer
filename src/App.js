c54332d7f6a79796702f3231dda52c4c6b0619ae
Tom Pan <tommmpn@gmail.com>
2022-02-28 10:55:35 -0500

    update test

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
       <div className="container">
         <Header onAdd = { () => setShowAddTask(!showAddTask) } showAdd = {showAddTask}/>
         <Routes>
-          <Route path = '/' exact element={
+          <Route path = '/' element={
             <>
               {showAddTask && <AddTask onAdd = {addTask} />}
               {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} />) : ('No Tasks To Show')}
